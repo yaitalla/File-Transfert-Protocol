82a028b3d0669251c978f3b0553fdb0c7dd4fce7
Lu <lubaujar@student.42.fr>
2018-11-07 09:54:36 +0100

    Update index.js

--- ftp/index.js
+++ ftp/index.js
@@ -1,6 +1,10 @@
 const upload = require('./upload/');
 const download = require('./download');
 
+Pour un router avec express, il faut redéclarer une instance de express:
+const api = express();
+après je te laisse chercher
+
 module.exports = (router) => {
   upload(router),
   download(router)
